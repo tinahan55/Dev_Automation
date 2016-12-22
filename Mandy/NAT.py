22452d93e5464217564836a579e9d2bc71952992
lileesqa <lileesqa@gmail.com>
2016-12-22 18:36:35 +0800

    add log, main

--- Mandy/NAT.py
+++ Mandy/NAT.py
@@ -1,3 +1,8 @@
+from lib.Device import *
+from lib.Configuration_add_dhcp import *
+import logging
+import os
+from time import gmtime, strftime
 
 class NAT(object):
     def __init__(self, type):
@@ -18,3 +23,40 @@ class NAT(object):
             commandlist.append("config classifier %s match ip protocol %s"%(classifier_index, protocol_type))
         commandlist.append("config dnat in-interface %s %s classifier %s translate-to ip %s port %s priority %s"%(in_interface, interface_index, classifier_index, ip, port, priority))
         return commandlist
+
+
+def set_log(filename, loggername):
+    logpath = os.path.join(os.getcwd(), 'log')
+    if not os.path.exists(logpath):
+        os.makedirs(logpath)
+    filepath = os.path.join(logpath, filename)
+    logger = logging.getLogger(loggername)
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fh = logging.FileHandler(filepath)
+    fh.setLevel(logging.INFO)
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+    console = logging.StreamHandler()
+    console.setLevel(logging.DEBUG)
+    console.setFormatter(formatter)
+    logger.addHandler(console)
+    return logger
+
+
+if __name__ == '__main__':
+    logfilename = "NAT%s.log"%(strftime("%Y%m%d%H%M", gmtime()))
+    logger = set_log(logfilename, "NAT_testing")
+    ip = "10.2.53.153"
+    port = "2222"
+    mode = "ssh"
+    username = "admin"
+    password = "admin"
+    app_engine_device = Device_Tool(ip, port, mode, username, password, "NAT")
+    if app_engine_device:
+        app_engine_device.device_get_version()
+        logger.info("Device Bios Version: %s"%(app_engine_device.bios_version))
+        logger.info("Lilee OS Version (build image): %s"%(app_engine_device.build_image))
+        logger.info("Recovery Image Version: %s"%(app_engine_device.boot_image))
+
+
